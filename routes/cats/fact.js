d08f7b4a4a498b2419b36843119df10183cb42de
mursal-r <mursalrahimi.ga@gmail.com>
2024-02-25 17:20:53 -0800

    cat-fact-app lab complete

---
+++ routes/cats/fact.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+const URL = 'https://catfact.ninja/fact'
+
+router.get('/', async function(req, res, next){
+    const catFact = await fetch(URL)
+        const catFactData = await catFact.json();
+        res.render('cat/fact', { catFact: catFactData.fact }) 
+})
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const URL = 'https://catfact.ninja/fact';
+
+/* GET home page. */ //axios needs to installed!!!! command: npm i axios****
+router.get('/', async function(req, res, next) {
+  try {
+    const response = await axios.get(URL);
+    const catFact = response.data.fact; 
+    res.render('index', { title: 'Cat Facts', catFact });
+  } catch (error) {
+    console.error('Error fetching cat fact:', error);
+    res.status(500).send('Error fetching cat fact');
+  }
 });
 
 module.exports = router;
